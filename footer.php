87deadca8f22f3b3150058b14be005e06628e40b
Tom Henderson <tomhenderson@mac.com>
2014-08-09 16:12:39 +1200

    Add sidebar for footer details.

--- footer.php
+++ footer.php
@@ -1,9 +1,7 @@
 </div>
 
 <div id="footer">
-  <p>level 1, imperial buildings, 44 queen street, auckland central, new zealand</p>
-  <p>po box 2183 shortland street, auckland cbd 1140</p>
-  <p>ph: 09 529 9177 | fax: 09 354 4362 | email: <a href="mailto:info@pack.co.nz">info@pack.co.nz</a></p>
+  <?php dynamic_sidebar( 'Footer' ); ?>
 </div>
 
 </div>
